99be8892b145c030c9d541f737aadc31881220fa
Daulet <Alana1507>
2020-08-27 11:46:55 -0700

    08-27 change

---
+++ 7 kyu/Two to One.js
@@ -0,0 +1,27 @@
+// Take 2 strings s1 and s2 including only letters from ato z.
+// Return a new sorted string, the longest possible, containing distinct letters,
+//
+//     each taken only once - coming from s1 or s2.
+//     Examples:
+// a = "xyaabbbccccdefww"
+// b = "xxxxyyyyabklmopq"
+// longest(a, b) -> "abcdefklmopqwxy"
+//
+// a = "abcdefghijklmnopqrstuvwxyz"
+// longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
+//
+// Test.assertEquals(longest("aretheyhere", "yestheyarehere"), "aehrsty")
+// Test.assertEquals(longest("loopingisfunbutdangerous", "lessdangerousthancoding"), "abcdefghilnoprstu")
+// Test.assertEquals(longest("inmanylanguages", "theresapairoffunctions"), "acefghilmnoprstuy")
+
+function longest(s1, s2) {
+    // your code
+    s3 = s1 + s2;
+    s4 = s3.split("");
+    s4 = s4.sort().filter(function (element, index, array) {
+        return element !== array[index - 1];
+    });
+    return s4.join("");
+}
+
+console.log(longest("loopingisfunbutdangerous", "lessdangerousthancoding"))
