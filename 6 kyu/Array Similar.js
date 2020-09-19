ef5c42bd267e76e93957b4bc853659eba05b04df
Daulet <Alana1507>
2020-09-18 23:26:36 -0700

    modify

--- 6 kyu/Array Similar.js
+++ 6 kyu/Array Similar.js
@@ -9,3 +9,9 @@
 // arraysSimilar(arr1, arr2); // Should equal true
 // arraysSimilar(arr2, arr3); // Should equal false
 // arraysSimilar(arr3, arr4); // Should equal false
+
+const arraysSimilar = (x, y) => {
+    if (x.length !== y.length) return false;
+    y = y.sort();
+    return x.sort().every((e, i) => e === y[i]);
+}
