d39ef8eebffda024ded3be85a969b26537a6ec16
daulet@yahoo.com <Alana1507>
2021-07-10 00:03:51 -0700

    7 july

--- test_tasks/new1.js
+++ test_tasks/new1.js
@@ -1,2 +1,14 @@
-let arr = ['daulet', 'bizhuminov']
-console.log(arr[1][1])
+// In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
+//
+//     Example:
+//
+// highAndLow("1 2 3 4 5");  // return "5 1"
+// highAndLow("1 2 -3 4 5"); // return "5 -3"
+// highAndLow("1 9 3 4 -5"); // return "9 -5"
+
+function highAndLow(numbers){
+    let minNum = Math.min(...numbers.split(' '))
+    let maxNum = Math.max(...numbers.split(' '))
+    return minNum.toString() +' ' + maxNum.toString()
+}
+console.log(highAndLow("1 2 3 4 5"))
