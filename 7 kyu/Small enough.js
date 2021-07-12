9b668f44ba1cd7b32b536fcd6ca78479e37c265e
daulet@yahoo.com <Alana1507>
2021-07-12 01:28:43 -0700

    12 july

--- 7 kyu/Small enough.js
+++ 7 kyu/Small enough.js
@@ -2,3 +2,16 @@
 // in the array are below or equal to the limit value. If they are, return true. Else, return false.
 //
 //     You can assume all values in the array are numbers.
+
+function smallEnough(a, limit){
+    for(let i = 0; i < a.length; i++) {
+        if (a[i] > limit) {
+            return false
+        }
+    }
+    
+    return true
+
+}
+
+console.log(smallEnough([78, 117, 110, 99, 104, 117, 107, 115], 100))
