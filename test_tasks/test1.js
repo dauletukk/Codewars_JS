048f5e4917f2a89ef99fe2765c92e48e72447321
daulet@yahoo.com <Alana1507>
2021-11-30 16:29:34 -0800

    delete and add

--- test_tasks/test1.js
+++
@@ -1,7 +0,0 @@
-function findShort(str) {
-    return str.split(' ').map(function (elem){
-        return elem.length
-    })
-}
-
-console.log(findShort("bitcoin take over the world maybe who knows perhaps"))
